0326abbe9b5ef7ef737b5bc971664f2d2d087299
Kamil <kamil55525@gmail.com>
2018-12-10 18:26:20 +0100

    Add tornadofx to dependencies and foundation for client UI

---
+++ src/main/kotlin/client/ui/ClientApp.kt
@@ -0,0 +1,5 @@
+package client.ui
+
+import tornadofx.App
+
+class App : App(AppView::class)
---
+++ src/main/kotlin/client/ui/View.kt
@@ -0,0 +1,10 @@
+package client.ui
+
+import tornadofx.View
+import tornadofx.vbox
+
+class AppView : View() {
+    override val root = vbox {
+
+    }
+}
