e392acd798fa67506bdc81e953a69ac008ee71da
Jakub Dąbek <jakub.dabek@gmail.com>
2018-12-12 21:17:13 +0100

    Move MessagingManager to common and add a little more error handling

---
+++ src/main/kotlin/common/MessagingManager.kt
@@ -0,0 +1,55 @@
+package common
+
+import java.io.*
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
+
+
+class MessagingManager(
+    val connectionId: Int,
+    private val inputStream: InputStream,
+    private val outputStream: OutputStream,
+    onMessageReceived: (connectionId: Int, Message) -> Unit,
+    onError: (connectionId: Int, ex: Exception?, fatal: Boolean) -> Boolean
+) {
+    private val objectInput: ObjectInputStream = ObjectInputStream(inputStream)
+    private val objectOutput: ObjectOutputStream = ObjectOutputStream(outputStream)
+    private val onMessageReceived: (Message) -> Unit = { onMessageReceived(connectionId, it) }
+    private val onError: (ex: Exception?, fatal: Boolean) -> Boolean = { ex, fatal -> onError(connectionId, ex, fatal) }
+    private val queue: BlockingQueue<Message> = LinkedBlockingQueue<Message>()
+
+
+    fun launch() {
+        while (true) {
+            try {
+                while (true) {
+                    val toSend = queue.poll(100, TimeUnit.MILLISECONDS) ?: break
+                    objectOutput.writeObject(toSend)
+                }
+                if (objectInput.available() > 0) {
+                    val message = objectInput.readObject() as Message
+                    onMessageReceived(message)
+                }
+            } catch (ex: ClassNotFoundException) {
+                onError(ex, true)
+                return
+            } catch (ex: ClassCastException) {
+                onError(ex, true)
+                return
+            } catch (ex: ObjectStreamException) {
+                onError(ex, true)
+                return
+            } catch (ex: IOException) {
+                onError(ex, true)
+                return
+            }
+        }
+    }
+
+    override fun equals(other: Any?) =
+        super.equals(other) || (other is MessagingManager && connectionId == other.connectionId)
+
+    override fun hashCode() = connectionId.hashCode()
+
+}
--- src/main/kotlin/server/CommunicationManager.kt
+++ src/main/kotlin/server/CommunicationManager.kt
@@ -1,18 +1,15 @@
 package server
 
 import common.Message
+import common.MessagingManager
 import common.Player
 import common.chinesecheckers.ChineseCheckerServerMessage
 import common.chinesecheckers.ChineseCheckersGameMessage
 import utility.nextUniqueInt
 import java.io.*
 import java.net.ServerSocket
-import java.net.Socket
-import java.net.SocketTimeoutException
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.TimeUnit
 import kotlin.random.Random
+
 
 class CommunicationManager {
     val connections = mutableListOf<MessagingManager>()
@@ -26,13 +23,15 @@ class CommunicationManager {
             while (true) {
                 val newSocket = listener.accept()
                 val connection = MessagingManager(
+                    Random.nextUniqueInt(connections.map { it.connectionId }),
                     newSocket.getInputStream(),
                     newSocket.getOutputStream(),
                     this::receiveMessage,
-                    this::handleMessagingError)
-                    TODO("map connections to players")
-                    // val id = Random.nextUniqueInt(playerMap.keys.map { it.id })
-                    // val player = Player(id, "User#$id")
+                    this::handleMessagingError
+                )
+                TODO("map connections to players")
+                // val id = Random.nextUniqueInt(playerMap.keys.map { it.id })
+                // val player = Player(id, "User#$id")
             }
         } catch (ex: IOException) {
 
@@ -59,51 +58,6 @@ class CommunicationManager {
             is ChineseCheckerServerMessage.ConnectionRequest -> TODO()
             is ChineseCheckerServerMessage.GameRequest -> TODO()
         }
-    }
-
-    inner class MessagingManager(
-        private val inputStream: InputStream,
-        private val outputStream: OutputStream,
-        onMessageReceived: (connectionId: Int, Message) -> Unit,
-        onError: (connectionId: Int, ex: Exception?, fatal: Boolean) -> Boolean
-    ) {
-        val connectionId: Int
-        private val objectInput: ObjectInputStream
-        private val objectOutput: ObjectOutputStream
-        val queue: BlockingQueue<Message> = LinkedBlockingQueue<Message>()
-        private val onMessageReceived: (Message) -> Unit
-        private val onError: (ex: Exception?, fatal: Boolean) -> Boolean
-
-        init {
-            connectionId = Random.nextUniqueInt(connections.map { it.connectionId})
-            objectInput = ObjectInputStream(inputStream)
-            objectOutput = ObjectOutputStream(outputStream)
-            this.onMessageReceived = { onMessageReceived(connectionId, it) }
-            this.onError = { ex, fatal -> onError(connectionId, ex, fatal) }
-        }
-
-        fun launch() {
-            while (true) {
-                try {
-                    while (true) {
-                        val toSend = queue.poll(100, TimeUnit.MILLISECONDS) ?: break
-                        objectOutput.writeObject(toSend)
-                    }
-                    val message = objectInput.readObject() as Message
-                    onMessageReceived(message)
-                } catch (ex: InterruptedIOException) {
-
-                } catch (ex: IOException) {
-                    onError(ex, true)
-                }
-            }
-        }
-
-        override fun equals(other: Any?) =
-            super.equals(other) || (other is MessagingManager && connectionId == other.connectionId)
-
-        override fun hashCode() = connectionId.hashCode()
-
     }
 
     inner class GameManager {
