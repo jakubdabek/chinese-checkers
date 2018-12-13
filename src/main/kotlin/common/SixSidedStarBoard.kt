7ae14f9fa1b09e72dc6696bfb3db319ead0259fd
Kamil <kamil55525@gmail.com>
2018-12-13 21:56:12 +0100

    Implement fill corners

--- src/main/kotlin/common/SixSidedStarBoard.kt
+++ src/main/kotlin/common/SixSidedStarBoard.kt
@@ -1,7 +1,17 @@
 package common
 
+import java.awt.Color
+
 class SixSidedStarBoard(val innerHexagonSizeLength: Int) {
     val fields: Map<HexCoord, Field>
+    val conditions = mapOf(
+        0 to { c: HexCoord -> c.z >=  innerHexagonSizeLength },
+        1 to { c: HexCoord -> c.y <= -innerHexagonSizeLength },
+        2 to { c: HexCoord -> c.x >=  innerHexagonSizeLength },
+        3 to { c: HexCoord -> c.z <= -innerHexagonSizeLength },
+        4 to { c: HexCoord -> c.y >=  innerHexagonSizeLength },
+        5 to { c: HexCoord -> c.x <= -innerHexagonSizeLength }
+    )
 
     init {
         if (innerHexagonSizeLength < 2)
@@ -10,8 +20,9 @@ class SixSidedStarBoard(val innerHexagonSizeLength: Int) {
         val m = mutableMapOf<HexCoord, Field>()
         for (x in -n..n) {
             for (y in -n..n) {
-                if ((x <  innerHexagonSizeLength && y <  innerHexagonSizeLength && x + y > -innerHexagonSizeLength) ||
-                    (x > -innerHexagonSizeLength && y > -innerHexagonSizeLength && x + y <  innerHexagonSizeLength)) {
+                if ((x < innerHexagonSizeLength && y < innerHexagonSizeLength && x + y > -innerHexagonSizeLength) ||
+                    (x > -innerHexagonSizeLength && y > -innerHexagonSizeLength && x + y < innerHexagonSizeLength)
+                ) {
                     m[HexCoord(x, y)] = Field()
                 }
             }
@@ -20,7 +31,13 @@ class SixSidedStarBoard(val innerHexagonSizeLength: Int) {
     }
 
     fun fillCorners(corners: Collection<Int>) {
-        TODO("Put pieces in specified corners")
+        for (corner in corners) {
+            for (field in fields) {
+                if (conditions[corner]?.invoke(field.key) == true) {
+                    field.value.piece = Piece(corner)
+                }
+            }
+        }
     }
 
     data class Piece(val cornerId: Int)
